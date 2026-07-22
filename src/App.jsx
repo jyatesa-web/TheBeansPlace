d976a4727fbed85d8500886ded072b969658edac
Johnathon Anderson <jyatesa@gmail.com>
2026-07-21 20:31:36 -0500

    modified AboutSection and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,7 @@ import RibbonTicker from "./components/RibbonTicker";
 import FeaturesSection from "./components/FeaturesSection";
 import ProductShowcase from "./components/ProductShowcase";
 import CtaSection from "./components/CtaSection"
+import AboutSection from "./components/AboutSection";
 
 export default function App() {
     return (
@@ -89,7 +90,7 @@ export default function App() {
             <section className="bg-cta"><CtaSection/></section>
 
             {/* ABOUT */}
-            <section className="bg-cta" id="about"></section>
+            <section className="bg-cta" id="about"><AboutSection/></section>
 
             {/* CONTACT */}
             <section className="bg-cta" id="contact"></section>
